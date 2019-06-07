92fd9751d9b40ffc66c9b563b418cc8c1478ffc7
Palwinder Singh <palwinder24@icloud.com>
2019-06-07 18:55:23 +0530

    homepage changes

--- if.js
+++
@@ -1,7 +0,0 @@
-var app= new Vue({
-    el:'#hero-text',
-    data:{
-        herotext:'Hi'
-        heroimage:'https://instagram.fluh2-1.fna.fbcdn.net/vp/aa42237d4b0a1ac7377d2f366a19d272/5D8BFB54/t51.2885-19/s320x320/60442225_2371396106434320_7135475047132561408_n.jpg?_nc_ht=instagram.fluh2-1.fna.fbcdn.net'
-    }
-})
